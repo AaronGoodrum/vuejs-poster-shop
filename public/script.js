f7a8d02d2d0899dc3cc6ac45a5da03391fb830d5
GoodZillo <ag505181@gmail.com>
2017-12-06 20:04:56 -0800

    building vue items to html

--- public/script.js
+++ public/script.js
@@ -1,1 +1,25 @@
-console.log('It works.');
+console.log('Server Started');
+	// create a new Vue instance plain property
+new Vue({
+	// Tell Vue where to be located/anchored in DOM (#id)
+	el:'#app',
+		data: {
+			total: 0,
+			items:[
+				{title: 'Item 1'},
+				{title: 'Item 2'},
+				{title: 'Item 3'},
+				{title: 'Item 4'}
+			],
+			cart: []
+		},
+	methods: {
+		addItem: function(index) {
+			console.log (index);
+			this.total += 9.99
+		}
+	}
+
+
+
+}); // VUE END
